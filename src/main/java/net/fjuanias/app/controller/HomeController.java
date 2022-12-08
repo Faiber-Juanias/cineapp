4f2bd602c6a7728c49702c39a6c3e5f1c88acc7c
fjuanias <faiberjuanias001@gmail.com>
2022-12-08 13:23:29 -0500

    Add home view and controller

--- src/main/java/net/fjuanias/app/controller/HomeController.java
+++ src/main/java/net/fjuanias/app/controller/HomeController.java
@@ -1,15 +1,69 @@
 package net.fjuanias.app.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.fjuanias.app.iservice.IPeliculaService;
+import net.fjuanias.app.model.Pelicula;
+import net.fjuanias.app.util.Utileria;
 
 @Controller
 public class HomeController {
+	
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	
+	@Autowired
+	private IPeliculaService servicePelicula;
 
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String goHome() {
 		return "home";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String mostrarPrincipal(Model model) {
+		List<String> listaFechas = Utileria.getNextDays(3);
+		List<Pelicula> peliculas = servicePelicula.buscarTodas();
+		
+		model.addAttribute("peliculas", peliculas);
+		model.addAttribute("fechaBusqueda", dateFormat.format(new Date()));
+		model.addAttribute("fechas", listaFechas);
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/detail/{id}/{fecha}", method = RequestMethod.GET)
+	public String mostrarDetalle(@PathVariable("id") int id, @PathVariable("fecha") Date fecha, Model model) {
+		model.addAttribute("pelicula", servicePelicula.buscarPorId(id));
+		return "detalle";
+	}
+	
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public String buscar(@RequestParam("fecha") Date fecha, Model model) {
+		List<String> listaFechas = Utileria.getNextDays(3);
+		List<Pelicula> peliculas = servicePelicula.buscarTodas();
+		
+		model.addAttribute("peliculas", peliculas);
+		model.addAttribute("fechaBusqueda", dateFormat.format(fecha));
+		model.addAttribute("fechas", listaFechas);
+		return "home";
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		webDataBinder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
 	}
 	
 }
