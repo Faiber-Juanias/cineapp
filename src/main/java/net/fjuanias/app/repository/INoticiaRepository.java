af931846b5b9242f336a1e23feb31304f57769bd
fjuanias <faiberjuanias001@gmail.com>
2022-12-16 16:33:41 -0500

    Update Noticia Repository, service and Model

---
+++ src/main/java/net/fjuanias/app/repository/INoticiaRepository.java
@@ -0,0 +1,20 @@
+package net.fjuanias.app.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.fjuanias.app.model.Noticia;
+
+@Repository
+public interface INoticiaRepository extends JpaRepository<Noticia, Integer> {
+
+	List<Noticia> findByEstatus(String estatus);
+	List<Noticia> findByFecha(Date fecha);
+	List<Noticia> findByEstatusAndFecha(String estatus, Date fecha);
+	List<Noticia> findByEstatusOrFecha(String estatus, Date fecha);
+	List<Noticia> findByFechaBetween(Date fecha1, Date fecha2);
+	
+}
--- src/main/java/net/fjuanias/app/service/NoticiaService.java
+++ src/main/java/net/fjuanias/app/service/NoticiaService.java
@@ -1,17 +1,21 @@
 package net.fjuanias.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.fjuanias.app.iservice.INoticiaService;
 import net.fjuanias.app.model.Noticia;
+import net.fjuanias.app.repository.INoticiaRepository;
 
 @Service
 public class NoticiaService implements INoticiaService {
+	
+	@Autowired
+	private INoticiaRepository noticiaRepo;
 
 	@Override
 	public void guardar(Noticia noticia) {
-		// TODO Auto-generated method stub
-
+		this.noticiaRepo.save(noticia);
 	}
 
 }
